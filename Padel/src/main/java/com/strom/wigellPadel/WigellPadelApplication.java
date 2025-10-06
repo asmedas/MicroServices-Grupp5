a7fe4395ea5060b1efd0954cf818232d44dccf51
MacStrom <marcus.strom123@gmail.com>
2025-10-06 17:54:44 +0200

    Testcommit för att se att kommande ändringar kommer hamna rätt i github.

--- Padel/src/main/java/com/strom/wigellPadel/WigellPadelApplication.java
+++ Padel/src/main/java/com/strom/wigellPadel/WigellPadelApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WigellPadelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WigellPadelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WigellPadelApplication.class, args);
+    }
 
 }
