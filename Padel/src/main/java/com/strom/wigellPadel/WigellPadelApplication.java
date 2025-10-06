0850f1fa233eef5fa3750120dc030622485f73b4
MacStrom <marcus.strom123@gmail.com>
2025-10-06 16:31:05 +0200

    Jag har skapat grunden till WigellPadel mikrotjänsten

---
+++ Padel/src/main/java/com/strom/wigellPadel/WigellPadelApplication.java
@@ -0,0 +1,13 @@
+package com.strom.wigellPadel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellPadelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellPadelApplication.class, args);
+	}
+
+}
