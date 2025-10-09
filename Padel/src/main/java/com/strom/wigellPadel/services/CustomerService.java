40512333bffed65ec19909fc03fce8e07d4bd78c
MacStrom <marcus.strom123@gmail.com>
2025-10-09 22:49:31 +0200

    Gjort en export med min keycloak realm-configuration och ordnat automatisk import när applicationen startas.

---
+++ Padel/src/main/java/com/strom/wigellPadel/services/CustomerService.java
@@ -0,0 +1,8 @@
+package com.strom.wigellPadel.services;
+
+public class CustomerService {
+
+    //TODO Fortsätt här!!!
+    //TODO Skapa även övriga services för Address, Court, Booking. När det är klart så är det Controllers som återstår.
+    //TODO Sedan se till så att ALLA endpoints fungerar
+}
