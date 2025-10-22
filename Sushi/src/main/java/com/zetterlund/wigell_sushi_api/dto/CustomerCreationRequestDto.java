c941a86e17951de46ff8bf8df2bad6a526f86c29
Tzakyra <tzakyra@gmail.com>
2025-10-22 16:54:11 +0200

    GET, POST och DELETE customer fungerar. La till fler exceptions

--- Sushi/src/main/java/com/zetterlund/wigell_sushi_api/dto/CustomerCreationRequestDto.java
+++ Sushi/src/main/java/com/zetterlund/wigell_sushi_api/dto/CustomerCreationRequestDto.java
@@ -1,32 +1,63 @@
 package com.zetterlund.wigell_sushi_api.dto;
 
+import java.util.List;
+
 public class CustomerCreationRequestDto {
     private String username;
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
     private String rawPassword;
+    private List<AddressDto> addresses;
 
     // Getters och setters
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public String getRawPassword() {
         return rawPassword;
     }
-
     public void setRawPassword(String rawPassword) {
         this.rawPassword = rawPassword;
+    }
+
+    public List<AddressDto> getAddresses() {
+        return addresses;
+    }
+    public void setAddresses(List<AddressDto> addresses) {
+        this.addresses = addresses;
     }
 }
