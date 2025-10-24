fb0d944633089e910b2eb5bb849f07bdd128113b
Tzakyra <tzakyra@gmail.com>
2025-10-24 13:43:53 +0200

    Dish fungerar och hämtas, renoverad converter - nytt namn CallCurrencyApiService. Knyckt från Sebbe med gott samvete. Anpassad till Sushi. Dish-kedjan renoverad efter ändringarna. Marcus kan också känna sig rånad med gott samvete.

--- Sushi/src/main/java/com/zetterlund/wigell_sushi_api/service/DishService.java
+++ Sushi/src/main/java/com/zetterlund/wigell_sushi_api/service/DishService.java
@@ -4,30 +4,37 @@ import com.zetterlund.wigell_sushi_api.dto.DishCreationRequestDto;
 import com.zetterlund.wigell_sushi_api.entity.Dish;
 import com.zetterlund.wigell_sushi_api.exception.ResourceNotFoundException;
 import com.zetterlund.wigell_sushi_api.repository.DishRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 public class DishService {
+    private static final Logger logger = LoggerFactory.getLogger(DishService.class);
     private final DishRepository dishRepository;
-    private final CurrencyConverterService currencyConverterService;
+    private final CallCurrencyApiService callCurrencyApiService;
 
-    public DishService(DishRepository dishRepository, CurrencyConverterService currencyConverterService) {
+    public DishService(DishRepository dishRepository, CallCurrencyApiService callCurrencyApiService) {
         this.dishRepository = dishRepository;
-        this.currencyConverterService = currencyConverterService;
+        this.callCurrencyApiService = callCurrencyApiService;
     }
 
     public List<Dish> getAllDishes() {
+        logger.info("getAllDishes service class");
+
         List<Dish> dishes = dishRepository.findAll();
         dishes.forEach(dish -> {
-            double convertedPrice = currencyConverterService.convertCurrency("JPY", dish.getPriceInSek());
+            BigDecimal convertedPrice = callCurrencyApiService.convertFromSEKToJPY(dish.getPriceInSek());
             dish.setPriceInJpy(convertedPrice);
         });
         return dishes;
     }
 
     public Dish addDish(Dish dish) {
+        logger.info("addDish service class");
         return dishRepository.save(dish);
     }
 
